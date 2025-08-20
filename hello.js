2d452fb4321a5bec975da7c454dc9df40233368a
Sudhir Kesharwani <sudhir@spxpert.com>
2025-08-20 20:19:35 +0530

    Checked in breaking changes from dev-sudhirke

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
 console.log("Hello, World!");
 console.log("This modification is done from dev-sudhrike branch.");
 console.log("Hello GitHub.");
+
+console.log("dev-sudhike - bad code,  this should be rolled back.");
