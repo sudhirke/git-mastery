f9e8d0aa15083ee3fcd1f42b828fdcf2cb5572db
Sudhir Kesharwani <sudhir@spxpert.com>
2025-08-20 20:37:00 +0530

    Pushing the dev-sudhirke to master

--- hello.js
+++ hello.js
@@ -1,6 +1,5 @@
 console.log("Hello, World!");
 console.log("This modification is done from dev-sudhrike branch.");
-<<<<<<< Updated upstream
 console.log("Hello GitHub.");
 
 //implementing new urgent fix
@@ -10,4 +9,3 @@ console.log("Trying out commit.");
 //i am in the process of implementing a new feature
 // This is a test file for git mastery.
 // dev-sudhirke - This file is used to demonstrate git operations.
->>>>>>> Stashed changes
