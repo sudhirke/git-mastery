595ccf48b29a04144554de4af5e30c7111e51fa5
Sudhir Kesharwani <sudhir@spxpert.com>
2025-08-20 20:20:40 +0530

    Revert "Checked in changes from dev-sudhirke"
    This reverts commit d34175f5398229abf85dd4692805743b6018132f.

--- hello.js
+++ hello.js
@@ -1,5 +1,3 @@
 console.log("Hello, World!");
 console.log("This modification is done from dev-sudhrike branch.");
 console.log("Hello GitHub.");
-
-console.log("dev-sudhike - bad code,  this should be rolled back.");
